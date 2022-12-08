f61d22da1d57cd03e288822187f29ec5215d778e
sivasurya16 <www.siva.com91@gmail.com>
2022-12-08 16:51:46 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,102 @@
+// let a=5;
+// console.log(a);
+
+// console.log(typeof(a));
+
+
+// let arr = ["red","blue","orange"];	
+// console.log(arr[0]);
+
+
+// let person = {
+// 	name:"aniruth",
+// 	age:15
+// }
+
+// console.log(person['age'])
+
+// let mark = 85;
+
+// if (mark > 90){
+// 	console.log("S");
+// }
+
+// else if (mark < 90 && mark>80){
+// 	console.log("A");
+// }
+
+// else {
+// 	console.log("B")
+// }
+
+
+// let num = 1;
+
+// switch(num){
+// 	case 0:
+// 		console.log("sunday");
+// 		break;
+
+// 	case 1:
+// 		console.log("monday");
+// 		break;
+	
+// 	case 2:
+// 		console.log("tuesday");
+// 		break;
+
+// 	case 3:
+// 		console.log("wednesday");
+// 		break;
+// }
+
+// for(let i=0;i<10;i++){
+// 	console.log(`hello ${i+1}`);
+// }
+
+// let i = 0;
+
+// while (i<10){
+// 	console.log(`hello ${i}`);
+// 	i++;
+// }
+
+
+// do {
+// 	console.log("hi");
+// 	i++;
+// }while(i<10);
+
+
+// let arr = ["red","blue","orange"];	
+
+// for (let color in arr){
+// 	console.log(arr[color]);
+// }
+
+// function greet(){
+// 	console.log("hello");
+// }
+
+// greet()
+
+
+// function sum(num1,num2){
+// 	let sum = num1+num2;
+// 	return sum;
+// }
+
+// let s = sum(3,4);
+// console.log(s);
+
+
+// let sum = function(num1,num2){
+// 	console.log(num1+num2);
+// }
+
+let sum = (num1,num2)=>{
+	let add = num1+num2;
+	console.log(add);
+}
+sum(10,20);
+
